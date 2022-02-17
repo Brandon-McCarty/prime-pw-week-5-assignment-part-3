800307a39ee4128697b333faf4612f0d54ab858a
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-02-17 15:37:11 -0600

    created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,26 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
+console.log(collection);
+
+function addToCollection(array, title, artist, yearPublished){
+    console.log('In addToCollection');
+    let album = {
+        Title: title,
+        Artist: artist,
+        yearPublished: yearPublished
+    }
+    array.push(album)
+}
+
+addToCollection(collection, 'Nightmare', 'Avenged Sevenfold', '2010')
+console.log(collection);
+
+addToCollection(collection, 'The Black Parade', 'My Chemical Romance', '2006')
+console.log(collection);
+
+addToCollection(collection, 'Tickets to My Downfall', 'Machine Gun Kelly', '2020')
+console.log(collection);
+
+addToCollection(collection, 'The Pick of Destiny', 'Tenacious D', '2006')
+console.log(collection);
